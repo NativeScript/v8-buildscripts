8436c2838f7dcf2a51d1d7953a1c9a824eb8af68
Vladimir Mutafov <vlado.mutafov@gmail.com>
2022-11-09 13:22:29 +0200

    use 13.0 as ios deployment target

--- scripts/build.sh
+++ scripts/build.sh
@@ -23,7 +23,7 @@ else
 fi
 
 if [[ ${PLATFORM} = "ios" ]]; then
-  GN_ARGS_BASE="${GN_ARGS_BASE} enable_ios_bitcode=false use_xcode_clang=true ios_enable_code_signing=false v8_enable_pointer_compression=false ios_deployment_target=${IOS_DEPLOYMENT_TARGET}"
+  GN_ARGS_BASE="${GN_ARGS_BASE} enable_ios_bitcode=false use_xcode_clang=true ios_enable_code_signing=false v8_enable_pointer_compression=false ios_deployment_target=\"${IOS_DEPLOYMENT_TARGET}\""
 elif [[ ${PLATFORM} = "android" ]]; then
   # Workaround v8 sysroot build issues with custom ndk
   GN_ARGS_BASE="${GN_ARGS_BASE} use_sysroot=false"
